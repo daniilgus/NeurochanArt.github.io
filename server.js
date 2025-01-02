8876a26453f3799835787fa7a109b5ad16c37fb6
Daniil Yusov <122688428+daniilgus@users.noreply.github.com>
2025-01-03 02:31:49 +0400

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(process.cwd(), 'public', 'app.html')); 
 });
 
-// Хранилище для идентификаторов загруженных изображений
-let loadedImageIds = new Set();
-
 async function getImages() {
     try {
         const response = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates`);
@@ -39,14 +36,6 @@ async function getImages() {
                 const photo = update.channel_post.photo[update.channel_post.photo.length - 1]; 
                 const fileId = photo.file_id;
 
-                // Проверяем, было ли это изображение уже загружено
-                if (loadedImageIds.has(fileId)) {
-                    continue; // Пропускаем, если изображение уже загружено
-                }
-
-                // Добавляем идентификатор в хранилище
-                loadedImageIds.add(fileId);
-
                 // Получаем информацию о файле
                 const fileResponse = await fetch(`https://api.telegram.org/bot${TOKEN}/getFile?file_id=${fileId}`);
                 if (!fileResponse.ok) {
@@ -58,36 +47,22 @@ async function getImages() {
                 const filePath = fileData.result.file_path;
                 const imageUrl = `https://api.telegram.org/file/bot${TOKEN}/${filePath}`;
 
-                // Проверка доступности файла
-                const imageCheckResponse = await fetch(imageUrl);
-                if (!imageCheckResponse.ok) {
-                    console.error(`Image not accessible at: ${imageUrl}`);
-                    continue; // Пропускаем недоступные изображения
-                }
-
                 const text = update.channel_post.text || update.channel_post.caption || "";
+
                 const authorMatch = text.match(/Автор:(.*)/);
                 const authorText = authorMatch ? authorMatch[1].trim() : "Неизвестный автор";
                 images.push({ url: imageUrl, text: text, author: authorText });
             }
         }
 
-        // Фильтрация изображений по их доступности в канале
-        const uniqueImages = images.filter((image, index, self) =>
-            index === self.findIndex((t) => (
-                t.url === image.url
-            ))
-        );
-
-        console.log('Extracted images:', uniqueImages); 
-        return uniqueImages;
+        console.log('Extracted images:', images); 
+        return images;
     } catch (error) {
         console.error('Error fetching images:', error.message);
         throw error;
     }
 }
 
-// Эндпоинт для получения изображений
 app.get('/getImages', async (req, res) => {
     try {
         const images = await getImages();
@@ -98,7 +73,6 @@ app.get('/getImages', async (req, res) => {
     }
 });
 
-// Запуск сервера
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
