e6f3d5ea7f9233e963a4043b4bc4d2373e391a17
Daniil Yusov <122688428+daniilgus@users.noreply.github.com>
2025-01-02 21:36:16 +0400

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 async function getImages() {
     try {
         console.log(`Fetching updates with token: ${TOKEN}`);
-        const response = await fetch(`https://api.telegram.org/bot${TOKEN}getUpdates`);
+        const response = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates`);
 
         if (!response.ok) {
             console.error(`HTTP error! status: ${response.status}`);
@@ -39,7 +39,7 @@ async function getImages() {
                 const photo = update.channel_post.photo[update.channel_post.photo.length - 1]; 
                 const fileId = photo.file_id;
 
-                const fileResponse = await fetch(`https://api.telegram.org/bot${TOKEN}getFile?file_id=${fileId}`);
+                const fileResponse = await fetch(`https://api.telegram.org/bot${TOKEN}/getFile?file_id=${fileId}`);
 
                 if (!fileResponse.ok) {
                     console.error(`HTTP error! status: ${fileResponse.status}`);
@@ -48,7 +48,7 @@ async function getImages() {
 
                 const fileData = await fileResponse.json();
                 const filePath = fileData.result.file_path;
-                const imageUrl = `https://api.telegram.org/file/bot${TOKEN}${filePath}`;
+                const imageUrl = `https://api.telegram.org/file/bot${TOKEN}/${filePath}`;
 
                 const username = update.channel_post.from ? update.channel_post.from.username || 'Неизвестный пользователь' : 'Неизвестный пользователь';
 
