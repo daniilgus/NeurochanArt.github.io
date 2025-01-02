bba0463182cadec02bc2041c8563e78444aec212
Daniil Yusov <122688428+daniilgus@users.noreply.github.com>
2025-01-03 01:19:11 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,28 +4,32 @@ const TOKEN = '8195705425:AAHjFZI_WI3xkXyGTqKDH3M8x67m48xAInc';
 const CHAT_ID = '-1002287069041'; 
 
 async function getImages() {
-    const response = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates`); 
+    const response = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates`);
     const data = await response.json();
 
     const images = [];
 
-    for (const update of data.result) { 
+    for (const update of data.result) {
         if (update.channel_post && update.channel_post.photo) {
-            const photo = update.channel_post.photo[update.channel_post.photo.length - 1]; 
+            const photo = update.channel_post.photo[update.channel_post.photo.length - 1];
             const fileId = photo.file_id;
 
             const fileResponse = await fetch(`https://api.telegram.org/bot${TOKEN}/getFile?file_id=${fileId}`);
             const fileData = await fileResponse.json();
             const filePath = fileData.result.file_path;
-            const text = update.channel_post.text || update.channel_post.caption || "";
-            const author = update.channel_post.from.username || "Неизвестный автор"; 
 
-            images.push({ url: `https://api.telegram.org/file/bot${TOKEN}/${filePath}`, text: text, author: author }); 
+            const text = update.channel_post.text || update.channel_post.caption || "";
+
+            const authorMatch = text.match(/Автор:(.*)/); 
+            const authorText = authorMatch ? authorMatch[1].trim() : "Неизвестный автор"; 
+
+            images.push({ url: `https://api.telegram.org/file/bot${TOKEN}/${filePath}`, text: text, author: authorText });
         }
     }
 
     return images;
 }
+
 
 
 getImages().then(images => {
