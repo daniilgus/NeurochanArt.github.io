7edc77607e416cd76f4fd4322daea5bea5f161ba
Daniil Yusov <122688428+daniilgus@users.noreply.github.com>
2025-01-02 21:20:16 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,49 @@
-const fetch = require('node\-fetch'); 
+const fetch = require('node-fetch'); 
 
 const TOKEN = '8195705425:AAHjFZI_WI3xkXyGTqKDH3M8x67m48xAInc';
 const CHAT_ID = '-1002287069041'; 
 
 async function getImages() {
-    const response = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates`); 
-    const data = await response.json();
+    try {
+        console.log(`Fetching updates with token: ${TOKEN}`);
+        const response = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates`);
 
-    const images = [];
-
-    for (const update of data.result) { 
-        if (update.channel_post && update.channel_post.photo) {
-            const photo = update.channel_post.photo[update.channel_post.photo.length - 1]; 
-            const fileId = photo.file_id;
-
-            const fileResponse = await fetch(`https://api.telegram.org/bot${TOKEN}/getFile?file_id=${fileId}`); 
-            const fileData = await fileResponse.json();
-            const filePath = fileData.result.file_path;
-
-            const imageUrl = `https://api.telegram.org/file/bot${TOKEN}/${filePath}`; 
-
-            const username = update.channel_post.from.username || 'Неизвестный пользователь';
-
-            images.push({ imageUrl, username }); 
+        if (!response.ok) {
+            console.error(`HTTP error! status: ${response.status}`);
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
-    }
 
-    return images;
+        const data = await response.json();
+        console.log('Received data from Telegram:', data);
+
+        const images = [];
+
+        for (const update of data.result) {
+            if (update.channel_post && update.channel_post.photo) {
+                const photo = update.channel_post.photo[update.channel_post.photo.length - 1]; 
+                const fileId = photo.file_id;
+
+                const fileResponse = await fetch(`https://api.telegram.org/bot${TOKEN}/getFile?file_id=${fileId}`);
+
+                if (!fileResponse.ok) {
+                    console.error(`HTTP error! status: ${fileResponse.status}`);
+                    throw new Error(`HTTP error! status: ${fileResponse.status}`);
+                }
+
+                const fileData = await fileResponse.json();
+                const filePath = fileData.result.file_path;
+                const imageUrl = `https://api.telegram.org/file/bot${TOKEN}/${filePath}`;
+                const username = update.channel_post.from.username || 'Неизвестный пользователь';
+
+                images.push({ imageUrl, username });
+            }
+        }
+
+        return images;
+    } catch (error) {
+        console.error('Error fetching images:', error.message);
+        throw error; // Это позволит поймать ошибку в обработчике маршрута
+    }
 }
 
 getImages().then(images => {
