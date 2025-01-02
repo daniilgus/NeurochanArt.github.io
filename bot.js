5e59e6646c09f819cdcd19a1d6cf176de13e4449
Daniil Yusov <122688428+daniilgus@users.noreply.github.com>
2025-01-03 03:05:20 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,21 +4,28 @@ const TOKEN = '8195705425:AAHjFZI_WI3xkXyGTqKDH3M8x67m48xAInc';
 const CHAT_ID = '-1002287069041'; 
 
 async function getImages() {
-    const response = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates`); 
+    const response = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates`);
     const data = await response.json();
 
     const images = [];
 
-    for (const update of data.result) { 
+    for (const update of data.result) {
         if (update.channel_post && update.channel_post.photo) {
-            const photo = update.channel_post.photo[update.channel_post.photo.length - 1]; 
+            const photo = update.channel_post.photo[update.channel_post.photo.length - 1];
             const fileId = photo.file_id;
 
             const fileResponse = await fetch(`https://api.telegram.org/bot${TOKEN}/getFile?file_id=${fileId}`);
             const fileData = await fileResponse.json();
             const filePath = fileData.result.file_path;
 
-            images.push(`https://api.telegram.org/file/bot${TOKEN}/${filePath}`); 
+            // Извлекаем текст сообщения или подпись
+            const text = update.channel_post.text || update.channel_post.caption || "";
+
+            // Ищем текст после "Автор"
+            const authorMatch = text.match(/Автор:(.*)/); // Регулярное выражение для поиска текста после "Автор:"
+            const authorText = authorMatch ? authorMatch[1].trim() : "Неизвестный автор"; // Если совпадение найдено, берем его, иначе - "Неизвестный автор"
+
+            images.push({ url: `https://api.telegram.org/file/bot${TOKEN}/${filePath}`, text: text, author: authorText });
         }
     }
 
