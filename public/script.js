ef30e065fdc9eccb16af7ae4669f97706eba5fdf
Daniil Yusov <122688428+daniilgus@users.noreply.github.com>
2025-01-03 19:55:18 +0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,33 @@
+async function loadArts() {
+    const artContainer = document.getElementById('artContainer');
+    artContainer.innerHTML = '';
+
+    try {
+        const response = await fetch('/getImages'); 
+        const images = await response.json();
+        console.log('Images received from server:', images); // Отладка
+
+        images.forEach(image => {
+            const artItem = document.createElement('div');
+            artItem.className = 'art-item';
+            artItem.setAttribute('data-text', `Автор: ${image.author}`);
+
+            const img = document.createElement('img');
+            img.src = image.url;
+            img.alt = "User  Art";
+            img.onclick = () => openModal(image.url, image.text);
+
+            artItem.appendChild(img);
+            artContainer.appendChild(artItem);
+        });
+    } catch (error) {
+        console.error('Error loading arts:', error);
+    }
+}
+
+
+function openModal(imageUrl, text) {
+    document.getElementById('modalImage').src = imageUrl;
+    document.getElementById('modalText').innerHTML = text; 
+    document.getElementById('modal').style.display = 'block';
+}
